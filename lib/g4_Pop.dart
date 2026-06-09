39bdb357ec42daaccaaf4ed2fae987f4ffc391c4
GuiScarparo <guilhermescarparogrande@gmail.com>
2026-06-09 09:22:26 -0300

    tema 4

---
+++ lib/g4_Pop.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main ()=> runApp(MaterialApp(home: TelaPrincipal()));
+
+class TelaPrincipal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tela inicial")),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Ir para a Detalhes"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TelaDetalhes(),
+            ),
+            );
+          },
+        )
+      )
+    ); 
+  }
+}
+class TelaDetalhes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tela Detalhes")),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Voltar para a Tela Inicial"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        )
+      )
+    ); 
+  }
+}
+            
---
+++ lib/g4_Replacement.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main ()=> runApp(MaterialApp(home: TelaLogin()));
+
+class TelaLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tela de Login")),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Entrar"),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TelaHome(),
+              ),
+            );
+          },
+        )
+      )
+    ); 
+  }
+}
+class TelaHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Tela Home")),
+      body: Center(
+        child: Text("Usuario logado com sucesso!"),
+      ),
+    ); 
+  }
+}
