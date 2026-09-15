5d0f6c0eca329359d078d606829453a230ca719b
GuiScarparo <guilhermescarparogrande@gmai.com>
2026-09-15 14:12:53 -0300

    atividades

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_world/main%20atividade%20SA02.dart';
+import 'package:hello_world/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('exibe a tela do acelerometro', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Acelerômetro'), findsOneWidget);
+    expect(find.text('PARADO'), findsOneWidget);
+    expect(find.text('Detecções: 0'), findsOneWidget);
   });
 }
